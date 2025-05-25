6a3fdca6b89044c68e1f0b05026f27378daebbd3
Michael Passmore <mpassmore14@yahoo.com>
2025-05-24 21:18:11 -0500

    in the process of reorganizing and cleaning up code

--- DELI-cious/src/main/java/AddChips.java
+++ DELI-cious/src/main/java/AddChips.java
@@ -1,3 +1,21 @@
 public class AddChips {
+    public AddChips(String chips) {
+        this.chips = chips;
+    }
+
+    public String getChips() {
+        return chips;
+    }
+
+    public void setChips(String chips) {
+        this.chips = chips;
+    }
+
     //select type of chips
+    String chips;
+    @Override
+    public String toString() {
+        return "And some  " + chips + " to go with it .";
+    }
+
 }
--- DELI-cious/src/main/java/AddSandwich.java
+++ DELI-cious/src/main/java/AddSandwich.java
@@ -1,9 +1,6 @@
+import java.util.Scanner;
 public class AddSandwich {
-    // walks user through options to create sandwich
-    // select your bread , sandwich size , toppings: users should be able to add extras of each topping
-    // toppings: meat , cheese, other toppings , select sauces
-    // would you like your sandwich toasted?
-    private String size;
+    private String size ;
 
     public AddSandwich() {
         this.size = size;
